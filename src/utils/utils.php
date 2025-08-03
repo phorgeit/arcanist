317f119cd505e1bf8a698bb05e87e2d08e194d22
amy bones <amy@spookygirl.boo>
2025-08-03 15:37:30 -0700

    Template idx() signature
    Summary:
    After @{function@arcanist:id}, `idx()` is one of the most commonly used utility functions in Phorge.
    Templating this, like the other templating diffs, helps improve code completion and type checking.
    
    Test Plan: Ran phpstan at level 3 and observed a decrease in error count.
    
    Reviewers: aklapper, O1 Blessed Committers, mainframe98
    
    Reviewed By: O1 Blessed Committers, mainframe98
    
    Subscribers: tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Differential Revision: https://we.phorge.it/D26361

--- src/utils/utils.php
+++ src/utils/utils.php
@@ -27,11 +27,13 @@ function id($x) {
  * a default if it does not. This function allows you to concisely access an
  * index which may or may not exist without raising a warning.
  *
- * @param   array   $array Array to access.
+ * @template T
+ * @template D
+ * @param   array<scalar, T> $array Array to access.
  * @param   scalar  $key Index to access in the array.
- * @param   wild    $default (optional) Default value to return if the key is
+ * @param   ?D      $default (optional) Default value to return if the key is
  *                  not present in the array.
- * @return  wild    If `$array[$key]` exists, that value is returned. If not,
+ * @return  T|D     If `$array[$key]` exists, that value is returned. If not,
  *                  $default is returned without raising a warning.
  */
 function idx(array $array, $key, $default = null) {
