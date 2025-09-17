385d08e5b8d8b924475a01cef1a1022701b8a62a
Andre Klapper <a9016009@gmx.de>
2025-09-17 18:49:32 +0200

    Provide a basic hint what Phage classes are about
    Summary: I found `../arcanist/bin/phage`. I did not know what it is until searching the interwebs. But many phings out there have a similar name. So I could provide pointers.
    
    Test Plan: None. I just read source file classes. So do you.
    
    Reviewers: O1 Blessed Committers, mainframe98
    
    Reviewed By: O1 Blessed Committers, mainframe98
    
    Subscribers: tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Differential Revision: https://we.phorge.it/D26356

--- src/phage/bootloader/PhageAgentBootloader.php
+++ src/phage/bootloader/PhageAgentBootloader.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Phage is a prototype parallel shell tool, like HyperShell.
+ * Phage is exposed as a wrapper around `bin/remote`.
+ * See https://web.archive.org/web/20241004053014/https://secure.phabricator.com/w/phacility_cluster/phage/
+ * or https://web.archive.org/web/20250423050457/https://secure.phabricator.com/T2794
+ */
 abstract class PhageAgentBootloader extends Phobject {
 
   abstract public function getName();
