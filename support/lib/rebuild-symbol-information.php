phorgeit/arcanist

commit 8fd7a39d741bcbcb189970e7e15cb7ae23ba3455

Regenerate symbol-information.json with a new script
Summary:
This script uses the underlying compatibility database that the
package used by the original script used. Rather than go through
the effort of installing it through composer, just download the
source directly and analyse the json files it provides. This
makes running the script really simple, and because no external
PHP code is executed, a lot safer.

In anticipation for traits and enums introduced in newer versions,
these have been added to the symbol-information.json file.
The compatibility database also records class constants, similar
to methods. These have also been added.

Closes T16274

Test Plan:
* Run rebuild-symbol-information.php
* Browse through a decade's worth of changes
* Despair at reviewing the changes (optional)

Reviewers: O1 Blessed Committers, avivey

Reviewed By: O1 Blessed Committers, avivey

Subscribers: avivey, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno

Maniphest Tasks: T16274

Differential Revision: https://we.phorge.it/D26448

support/lib/rebuild-symbol-information.php
@@ -1,0 +1,271 @@
+#!/usr/bin/env php
+<?php
+
+$root = dirname(dirname(dirname(__FILE__)));
+require_once $root.'/support/init/init-script.php';
+
+$args = new PhutilArgumentParser($argv);
+$args->setTagline(pht('rebuild the symbol-information.json file'));
+$args->setSynopsis(<<<EOHELP
+    **rebuild-map.php** [__options__] __root__
+        Rebuild the symbol-information.json file.
+
+EOHELP
+);
+
+$args->parseStandardArguments();
+$args->parse(
+  array(
+    array(
+      'name'      => 'version',
+      'param'     => 'version',
+      'default'   => 'latest',
+      'help'      => pht('Use a specific version of llavile/php-compatinfo-db'),
+    ),
+    array(
+      'name'      => 'show',
+      'help'      => pht(
+        'Print symbol information to stdout instead of writing it to '.
+        'the symbol-information.json file.'),
+    ),
+  ));
+
+const REPO = 'https://github.com/llaville/php-compatinfo-db';
+
+$version = $args->getArg('version');
+$target = Filesystem::createTemporaryDirectory();
+
+if (extension_loaded('zip')) {
+  $download_path = $target.'/compatinfo-db-'.$version.'.zip';
+  id(new PhutilGitHubReleaseDownloader(REPO, $download_path))
+    ->setDownloadFormat('zip')
+    ->setVersion($version)
+    ->download();
+
+  $zip = new ZipArchive();
+  $result = $zip->open($download_path);
+  if (!$result) {
+    throw new Exception(
+      pht(
+        'Opening %s failed! %s.',
+        $download_path,
+        $result === false ? 'Unknown Error' : (string)$result));
+  }
+
+  $zip->extractTo($target);
+
+  if ($version === 'latest') {
+    $target .= '/php-compatinfo-db-master';
+  } else {
+    $target .= '/php-compatinfo-db-'.$version;
+  }
+} else if (extension_loaded('phar') && extension_loaded('zlib')) {
+  $download_path = $target.'/compatinfo-db-'.$version.'.tar.gz';
+  id(new PhutilGitHubReleaseDownloader(REPO, $download_path))
+    ->setDownloadFormat('tar.gz')
+    ->setVersion($version)
+    ->download();
+
+  id(new PharData($download_path))->extractTo($target, null, true);
+
+  if ($version === 'latest') {
+    $target .= '/php-compatinfo-db-master';
+  } else {
+    $target .= '/php-compatinfo-db-'.$version;
+  }
+} else if (Filesystem::binaryExists('git')) {
+  git_checkout($target, $version);
+} else {
+  throw new Exception(
+    pht('No viable means to download llaville/compatinfo-db is available.'));
+}
+
+$iterator = new RecursiveDirectoryIterator(
+  $target.'/data/reference/extension',
+  FilesystemIterator::SKIP_DOTS |
+  FilesystemIterator::CURRENT_AS_FILEINFO |
+  FilesystemIterator::KEY_AS_PATHNAME |
+  FilesystemIterator::UNIX_PATHS);
+
+$symbol_information = array(
+  '@'.'generated' => true,
+  'params' => array(),
+  'functions' => array(),
+  'classes' => array(),
+  'interfaces' => array(),
+  'constants' => array(),
+  'traits' => array(),
+  'methods' => array(),
+  'static_methods' => array(),
+  'class_constants' => array(),
+  // TODO: Can we get this from PHP CompatInfo?
+  // See https://github.com/llaville/php-compat-info/issues/185.
+  'functions_windows' => array(
+    'apache_child_terminate' => false,
+    'chroot' => false,
+    'lchgrp' => false,
+    'lchown' => false,
+    'nl_langinfo' => false,
+    // Deprecated in PHP 8.1
+    'strptime' => false,
+    'sys_getloadavg' => false,
+    'checkdnsrr' => '5.3.0',
+    'dns_get_record' => '5.3.0',
+    'fnmatch' => '5.3.0',
+    'getmxrr' => '5.3.0',
+    'getopt' => '5.3.0',
+    'imagecolorclosesthwb' => '5.3.0',
+    'inet_ntop' => '5.3.0',
+    'inet_pton' => '5.3.0',
+    'link' => '5.3.0',
+    'linkinfo' => '5.3.0',
+    'readlink' => '5.3.0',
+    'socket_create_pair' => '5.3.0',
+    'stream_socket_pair' => '5.3.0',
+    'symlink' => '5.3.0',
+    'time_nanosleep' => '5.3.0',
+    'time_sleep_until' => '5.3.0',
+    // php/doc-en@2a0e3aa7bed3a46b9cc052701579ab2efa1bbc26
+    'imagecreatefromxpm' => '5.3.19',
+    // https://www.php.net/manual/en/migration70.changed-functions.php
+    'getrusage' => '7.0.0',
+    // https://www.php.net/manual/en/migration72.new-features.php
+    'proc_nice' => '7.2.0',
+  ),
+);
+
+/** @var SplFileInfo $fileinfo */
+foreach (new RecursiveIteratorIterator($iterator) as $path => $fileinfo) {
+  if ($fileinfo->isDir()) {
+    continue;
+  }
+
+  $extension_name = basename(dirname($fileinfo->getPath()));
+  $entries = phutil_json_decode(Filesystem::readFile($path));
+
+  switch ($fileinfo->getFilename()) {
+    case 'classes.json:':
+      $symbol_information['classes'] = map_entries($extension_name, $entries);
+      break;
+    case 'const.json':
+      foreach ($entries as $constant) {
+        $class = $constant['class_name'];
+        $name = $constant['name'];
+
+        $symbol_information['class_constants'][$class][$name] = map_entry(
+          $extension_name,
+          $constant);
+      }
+      break;
+    case 'constants.json':
+      $symbol_information['constants'] = map_entries($extension_name, $entries);
+      break;
+    case 'functions.json':
+      foreach ($entries as $function) {
+        $name = $function['name'];
+        $parameters = idx($function, 'parameters', '');
+        // Deprecations can be a string or an array, with an optional message
+        // key that contains the reason.
+        $deprectated = idx($function, 'deprecated', '');
+
+        if ($parameters !== '') {
+          foreach (explode(',', $parameters) as $parameter) {
+            $symbol_information['params'][$name][] = trim($parameter);
+          }
+        }
+
+        // Skip entries about moved functions as they tend to overwrite and
+        // result in misleading information.
+        if (
+          is_array($deprectated) &&
+          isset($deprectated['message']) &&
+          strpos($deprectated['message'], 'was moved') !== false) {
+          continue;
+        }
+
+        $symbol_information['functions'][$name] = map_entry(
+          $extension_name,
+          $function);
+      }
+      break;
+    case 'methods.json':
+      foreach ($entries as $method) {
+        $class = $method['class_name'];
+        $name = $method['name'];
+
+        if (!empty($method['static'])) {
+          $symbol_information['static_methods'][$class][$name] = map_entry(
+            $extension_name,
+            $method);
+        } else {
+          $symbol_information['methods'][$class][$name] = map_entry(
+            $extension_name,
+            $method);
+        }
+      }
+      break;
+    case 'interfaces.json':
+      $symbol_information['interfaces'] = map_entries(
+        $extension_name,
+        $entries);
+      break;
+    case 'traits.json':
+      $symbol_information['traits'] = map_entries($extension_name, $entries);
+      break;
+  }
+}
+
+ksort($symbol_information['params']);
+ksort($symbol_information['functions']);
+ksort($symbol_information['classes']);
+ksort($symbol_information['interfaces']);
+ksort($symbol_information['constants']);
+ksort($symbol_information['traits']);
+ksort($symbol_information['methods']);
+ksort($symbol_information['static_methods']);
+ksort($symbol_information['class_constants']);
+
+if ($args->getArg('show')) {
+  echo id(new PhutilJSON())->encodeFormatted($symbol_information);
+} else {
+  Filesystem::writeFile(
+    $root.'/resources/php/symbol-information.json',
+    id(new PhutilJSON())->encodeFormatted($symbol_information));
+
+  echo pht('Done.')."\n";
+}
+
+Filesystem::remove($target);
+
+function map_entries(string $extension_name, array $entries) {
+  $information = array();
+
+  foreach ($entries as $entry) {
+    $name = $entry['name'];
+    $information[$name] = map_entry($extension_name, $entry);
+  }
+
+  return $information;
+}
+
+function map_entry(string $extension_name, array $entry) {
+  return array(
+    'ext.name' => $extension_name,
+    'ext.min'  => idx($entry, 'ext_min'),
+    'ext.max'  => idx($entry, 'ext_max'),
+    'php.min'  => idx($entry, 'php_min'),
+    'php.max'  => idx($entry, 'php_max'),
+  );
+}
+
+function git_checkout($target, $version) {
+  if ($version === 'latest') {
+    execx('git clone --single-branch --depth 1 %s %s', REPO, $target);
+  } else {
+    execx(
+      'git clone --single-branch --depth 1 --branch %s %s %s',
+      'v'.$version,
+      REPO,
+      $target);
+  }
+}

commit 2e4627ca77a92a6b6afbe0038a56fb70fb1b0a80

Fix overwritten entries in symbol-information.json
Summary: The changes in 8fd7a39d741b accidentally overwrote entries for constants and classes for each extension.

Test Plan:
* Run unit tests
* Look at the changes in symbol-information.json, under `constants` and `classes`

Reviewers: O1 Blessed Committers, aklapper

Reviewed By: O1 Blessed Committers, aklapper

Subscribers: aklapper, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno

Differential Revision: https://we.phorge.it/D26882

support/lib/rebuild-symbol-information.php
@@ -144,8 +144,37 @@ foreach (new RecursiveIteratorIterator($iterator) as $path => $fileinfo) {
   $entries = phutil_json_decode(Filesystem::readFile($path));
 
   switch ($fileinfo->getFilename()) {
-    case 'classes.json:':
-      $symbol_information['classes'] = map_entries($extension_name, $entries);
+    case 'classes.json':
+      foreach ($entries as $class) {
+        $name = $class['name'];
+        $symbol_information['classes'][$name] = map_entry(
+          $extension_name,
+          $class);
+      }
+      break;
+    case 'interfaces.json':
+      foreach ($entries as $interface) {
+        $name = $interface['name'];
+        $symbol_information['interfaces'][$name] = map_entry(
+          $extension_name,
+          $interface);
+      }
+      break;
+    case 'traits.json':
+      foreach ($entries as $trait) {
+        $name = $trait['name'];
+        $symbol_information['traits'][$name] = map_entry(
+          $extension_name,
+          $trait);
+      }
+      break;
+    case 'constants.json':
+      foreach ($entries as $constant) {
+        $name = $constant['name'];
+        $symbol_information['constants'][$name] = map_entry(
+          $extension_name,
+          $constant);
+      }
       break;
     case 'const.json':
       foreach ($entries as $constant) {
@@ -156,9 +185,6 @@ foreach (new RecursiveIteratorIterator($iterator) as $path => $fileinfo) {
           $extension_name,
           $constant);
       }
-      break;
-    case 'constants.json':
-      $symbol_information['constants'] = map_entries($extension_name, $entries);
       break;
     case 'functions.json':
       foreach ($entries as $function) {
@@ -232,14 +258,6 @@ foreach (new RecursiveIteratorIterator($iterator) as $path => $fileinfo) {
         }
       }
       break;
-    case 'interfaces.json':
-      $symbol_information['interfaces'] = map_entries(
-        $extension_name,
-        $entries);
-      break;
-    case 'traits.json':
-      $symbol_information['traits'] = map_entries($extension_name, $entries);
-      break;
   }
 }
 
@@ -264,17 +282,6 @@ if ($args->getArg('show')) {
 }
 
 Filesystem::remove($target);
-
-function map_entries(string $extension_name, array $entries) {
-  $information = array();
-
-  foreach ($entries as $entry) {
-    $name = $entry['name'];
-    $information[$name] = map_entry($extension_name, $entry);
-  }
-
-  return $information;
-}
 
 function map_entry(string $extension_name, array $entry) {
   return array(

src/lint/linter/phpast/rules/__tests__/php-compatibility/php71-features.lint-test
@@ -24,6 +24,7 @@ error:5:19:PHPAST45:PHP Compatibility
 error:8:3:PHPAST45:PHP Compatibility
 error:10:27:PHPAST45:PHP Compatibility
 error:10:41:PHPAST45:PHP Compatibility
+error:14:13:PHPAST45:PHP Compatibility
 error:15:10:PHPAST45:PHP Compatibility
 error:19:6:PHPAST45:PHP Compatibility
 ~~~~~~~~~~